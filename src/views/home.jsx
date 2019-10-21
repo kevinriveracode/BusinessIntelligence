8f2982fe1af6d615b5fda3ad8c3385d21759ad0f
Kevin <kevinrivera.a4b@gmail.com>
2019-10-21 15:41:29 +0200

    maquetacion de header

--- src/views/home.jsx
+++ src/views/home.jsx
@@ -1,9 +1,9 @@
 import React, {Component} from 'react';
-
+import '../sass/header.scss';
 class Header extends Component {
   render() {
     return (
-        <header>
+        <header className="Header">
           <div className="container-logo">
             <img className="img-logo" src="#" />
             <p className="reference-logo">
@@ -11,13 +11,14 @@ class Header extends Component {
               <h3 className="subtitle">AGENCIA DE SOLUCIONES TECNOLOGICAS</h3>
             </p>
           </div>
-          <div>
+          <div className="primary-menu">
             <nav>
               <li>Inicio</li>
               <li>Servicios</li>
               <li>Portafolio</li>
               <li>Nosotros</li>
               <li>Contacto</li>
+              <li><img src="/comercio-y-compras.svg" alt=""/></li>
             </nav>
           </div>
         </header>
