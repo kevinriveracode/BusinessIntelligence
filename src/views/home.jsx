66d8f898dbdd69d774e7bc2230d6534b706e5f1f
Kevin <kevinrivera.a4b@gmail.com>
2019-10-21 13:53:54 +0200

    add maquetacion menu

--- src/views/home.jsx
+++ src/views/home.jsx
@@ -1,11 +1,38 @@
 import React, {Component} from 'react';
-class Home extends Component{
-  render(){
-    return(
-        <section>
 
-        </section>
-    )
+class Header extends Component {
+  render() {
+    return (
+        <header>
+          <div className="container-logo">
+            <img className="img-logo" src="#" />
+            <p className="reference-logo">
+              <h1 className="title">Business Intelligence</h1>
+              <h3 className="subtitle">AGENCIA DE SOLUCIONES TECNOLOGICAS</h3>
+            </p>
+          </div>
+          <div>
+            <nav>
+              <li>Inicio</li>
+              <li>Servicios</li>
+              <li>Portafolio</li>
+              <li>Nosotros</li>
+              <li>Contacto</li>
+            </nav>
+          </div>
+        </header>
+    );
   }
 }
+
+class Home extends Component {
+  render() {
+    return (
+        <section>
+          <Header/>
+        </section>
+    );
+  }
+}
+
 export default Home;
