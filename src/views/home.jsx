f522d7fb72921d4e5080b1c9216c8b4262e5fafb
Kevin <kevinrivera.a4b@gmail.com>
2019-10-21 15:56:19 +0200

    add menu app

--- src/views/home.jsx
+++ src/views/home.jsx
@@ -5,7 +5,6 @@ class Header extends Component {
     return (
         <header className="Header">
           <div className="container-logo">
-            <img className="img-logo" src="#" />
             <p className="reference-logo">
               <h1 className="title">Business Intelligence</h1>
               <h3 className="subtitle">AGENCIA DE SOLUCIONES TECNOLOGICAS</h3>
@@ -18,7 +17,11 @@ class Header extends Component {
               <li>Portafolio</li>
               <li>Nosotros</li>
               <li>Contacto</li>
-              <li><img src="/comercio-y-compras.svg" alt=""/></li>
+            </nav>
+          </div>
+          <div className="app-menu">
+            <nav>
+              {/*Aqui se pondra el acceso a backoffice, al checkout de compra*/}
             </nav>
           </div>
         </header>
