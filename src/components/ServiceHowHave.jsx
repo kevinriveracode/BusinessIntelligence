fa70c1b742fba073b3eb8f9ccf81aa06756d6196
Kevin <kevinrivera.a4b@gmail.com>
2019-11-06 10:52:48 +0100

    diseño web

--- src/components/ServiceHowHave.jsx
+++ src/components/ServiceHowHave.jsx
@@ -6,30 +6,19 @@ class ServiceHowHave extends Component{
         <section className="ServiceHowHave">
           <div>
             <div className="title-section">
-              <span>DISEÑO DE TIENDAS ONLINE</span>
-              <h2>CARACTERÍSTICAS PRINCIPALES DE LAS TIENDAS ONLINE QUE DISEÑAMOS</h2>
+              <span>{this.props.section}</span>
+              <h2>{this.props.sectiondes}</h2>
             </div>
             <div className="content-section">
-              <div>
-                <img src="" alt="image"/>
-                <h3>Tiendas online profesionales y económicas</h3>
-                <p>Desarrollamos tu tienda online con todo lo que necesitas, con un diseño personalizado a medida</p>
-              </div>
-              <div>
-                <img src="" alt="image"/>
-                <h3>Tendrás una tienda online totalmente autogestionable</h3>
-                <p>Te enseñamos la gestión básica de tu tienda online, para que puedas llevarla tú, sin depender de nadie.</p>
-              </div>
-              <div>
-                <img src="" alt="image"/>
-                <h3>La tienda online será de tu propiedad, 100% a tu nombre</h3>
-                <p>La web está alojada en un servidor y con un dominio a tu nombre, por lo que no estarás atado a nadie.</p>
-              </div>
-              <div>
-                <img src="" alt="image"/>
-                <h3>Diseño de tiendas online responsive adaptado</h3>
-                <p>Ya no vale cualquier diseño, la tienda online debe ser dinámica y adaptada a cualquier pantalla.</p>
-              </div>
+              {this.props.items.map((item)=> {
+                return(
+                    <div>
+                      <img src="" alt="image"/>
+                      <h3>{item.title}</h3>
+                      <p>{item.description}</p>
+                    </div>
+                );
+              })}
             </div>
           </div>
         </section>
