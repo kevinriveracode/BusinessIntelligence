bb8ec5b08fdf2c1f8e74edbe648ebf6174508c94
Kevin <kevinrivera.a4b@gmail.com>
2019-11-21 18:05:43 +0100

    .

--- src/components/item-info.jsx
+++ src/components/item-info.jsx
@@ -3,10 +3,10 @@ import '../sass/utils.scss';
 class ItemInfo extends Component{
   render(){
     return(
-        <div className="w-30 u-flex-col ">
+        <div className="w-30 u-flex-col al-center">
           <img className={'img-responsive'} src={this.props.image} alt=""/>
           <h3>{this.props.name}</h3>
-          <p>{this.props.description}</p>
+          <p className='pad-tex text-center'>{this.props.description}</p>
         </div>
     )
   }
