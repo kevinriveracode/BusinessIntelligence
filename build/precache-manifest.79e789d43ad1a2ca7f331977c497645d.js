4a617207326837dcfa241af5a748c995f74435a8
Kevin <kevinrivera.a4b@gmail.com>
2019-11-22 13:45:15 +0100

    .

---
+++ build/precache-manifest.79e789d43ad1a2ca7f331977c497645d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9966c39760afb69b415368506f03c69e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d6a5b4dd0622c56a4549",
+    "url": "./static/css/main.40d7778d.chunk.css"
+  },
+  {
+    "revision": "cdd8bc008c8ea641b034",
+    "url": "./static/js/2.7326b4e8.chunk.js"
+  },
+  {
+    "revision": "d6a5b4dd0622c56a4549",
+    "url": "./static/js/main.4d8e115f.chunk.js"
+  },
+  {
+    "revision": "9584610d8c5fb88d533f",
+    "url": "./static/js/runtime-main.fa5f1e83.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "./static/media/logo.25bf045c.svg"
+  }
+]);
